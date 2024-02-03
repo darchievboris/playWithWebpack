f51faddff48d9248c0a29745c23dd772de8428eb
darchievboris <darchievboris@gmail.com>
2024-02-03 17:25:22 +0300

    added css modules and 'global' for reading by ts

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,22 +1,31 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {ModuleOptions} from 'webpack'
 import {BuildOptions} from "./types/types";
-export function buildLoaders(options:BuildOptions):ModuleOptions['rules']{
+
+export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === "development"
 
+    const cssLoaderWithModules = {
+        loader: "css-loader",
+        options: {
+            modules: {
+                localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+            }
+        }
+    }
     const scssLoader = {
-            test: /\.s[ac]ss$/i,
-            use: [
-                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader",
-            ],
-        }
-        const tsxLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            cssLoaderWithModules,
+            "sass-loader",
+        ],
+    }
+    const tsxLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
     return [
         scssLoader,
         tsxLoader
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import './App.scss'
+import classes from './App.module.scss'
 export const App = () => {
     const [counter, setCounter] = useState<number>(0);
     const increment = () => setCounter(prev=>prev+1)
     return (
         <div>
-            <h1>{counter}</h1>
-            <button onClick={increment}>inc</button>
+            <h1 className={classes.value}>{counter}</h1>
+            <button className={classes.button} onClick={increment}>inc</button>
         </div>
     );
 };
