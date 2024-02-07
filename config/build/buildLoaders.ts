320413bf17b554b701987b2ee5b6b8137ba9cb57
darchievboris <darchievboris@gmail.com>
2024-02-07 15:03:53 +0300

    added assets of img,png and svg with loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -26,8 +26,37 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
         use: 'ts-loader',
         exclude: /node_modules/,
     }
+    const assetsLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+    }
+    const svgrLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig:{
+                        plugins:[
+                            {
+                                name:'convertColors',
+                                params:{
+                                    currentColor:true,
+                                }
+                            }
+                        ]
+                    }
+
+                }
+            }
+        ],
+    }
     return [
+        assetsLoader,
         scssLoader,
-        tsxLoader
+        tsxLoader,
+        svgrLoader
     ]
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,18 @@ import React, {useState} from 'react';
 import classes from './App.module.scss'
 import {Link, Outlet} from "react-router-dom";
 import About from "@/pages/about/About";
+import imgJpg from "@/assets/img1.jpg";
+import imgPng from "@/assets/img3.png";
+import ImgSvg from "@/assets/img4.svg";
+
 export const App = () => {
     const [counter, setCounter] = useState<number>(0);
-    const increment = () => setCounter(prev=>prev+1)
+    const increment = () => setCounter(prev => prev + 1)
     return (
         <div>
+            <div><img width={100} height={100} src={imgPng}/></div>
+            <div><img width={100} height={100} src={imgJpg}/></div>
+            <div><ImgSvg style={{color:'green'}} width={50} height={50} fill={'red'}/></div>
             <Link to={'/about'}>about</Link>
             <br/>
             <Link to={'/shop'}>shop</Link>
